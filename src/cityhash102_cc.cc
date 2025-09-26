c3c4ecee7243cfcc919ce5225aa5ba7bdbd75441
Michael Freeman <mfreeman451@gmail.com>
2025-09-26 12:19:26 -0500

    relaxing build requirements around mirage

--- src/cityhash102_cc.cc
+++ src/cityhash102_cc.cc
@@ -1,4 +1,12 @@
 // Compile ClickHouse's CityHash v1.0.2 into the stubs archive
-// This file just includes the upstream implementation unit.
+// Force the portable code path so the build does not depend on SSE4.2 intrinsics.
+
+#ifndef CITYHASH_FORCE_PORTABLE
+#define CITYHASH_FORCE_PORTABLE
+#endif
+
+#ifdef __SSE4_2__
+#undef __SSE4_2__
+#endif
 
 #include "city.cc"
